5ecaef81f6fda1aed7aee000371023110ae8b4a6
AryaAzadeh <aryazdh@gmail.com>
2023-07-23 09:55:05 +0330

    Filter Tours
    Filter Tours with dateFrom-DateTo PriceFrom-PriceTo sortBy-sortOrder

---
+++ app/Http/Requests/TourListRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class TourListRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'dateFrom' => 'date',
+            'dateTo' => 'date',
+            'priceFrom' => 'numeric',
+            'priceTo' => 'numeric',
+            'sortBy' => Rule::in(['price']),
+            'sortOrder' => Rule::in(['desc' , 'ase']),
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+          'sortBy' => 'just price accepted'  ,
+          'sortOrder' => "just 'desc' or 'ase' accepted"  ,
+        ];
+    }
+}
