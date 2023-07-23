3698f2240e1090850dc8e1377e9a38626ec4fc4a
AryaAzadeh <aryazdh@gmail.com>
2023-07-23 12:45:56 +0330

    add admin endpoint
    add admin endpoint and sanctum auth system
    add travel store route with middleware
    and role check middleware

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Auth\LoginController;
 use App\Http\Controllers\Api\V1\TourController;
 use App\Http\Controllers\Api\V1\TravelController;
 use Illuminate\Http\Request;
@@ -24,4 +25,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::prefix('v1')->group(function (){
    Route::get('travels' , [TravelController::class , 'index']);
    Route::get('travels/{travel:slug}/tours' , [TourController::class , 'index']);
+
+   Route::prefix('admin')->middleware(['auth:sanctum' , 'role:admin'])->group(function () {
+      Route::post('store/travel' , [\App\Http\Controllers\Api\V1\Admin\TravelController::class , 'store']);
+   });
 });
+
+Route::post('login' , LoginController::class)->name('login');
